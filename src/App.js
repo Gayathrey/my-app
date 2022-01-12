1398f9774e138c476bd544848f895c757c9e85c7
Gayathrey <72188275+Gayathrey@users.noreply.github.com>
2022-01-12 12:12:18 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import OneSignal from "react-onesignal"
+
+function App() {
+  
+  useEffect(() => {
+    OneSignal.init({
+      appId: process.env.REACT_APP_ONESIGNAL,
+    });
+  }, []);
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
